baf1c38cef8d7405872d2c9b2e2c9d808ea41126
shreyanshkotak <sk7739@nyu.edu>
2026-04-26 22:49:57 -0400

    feat: enable text content prop in content mode

--- apps/builder/app/builder/features/settings-panel/props-section/props-section.stories.tsx
+++ apps/builder/app/builder/features/settings-panel/props-section/props-section.stories.tsx
@@ -402,6 +402,7 @@ export const PropsSection = () => {
             instanceId={instanceId}
             propsLogic={logic}
             propValues={new Map()}
+            propValuesByInstanceSelector={new Map()}
             component="Button"
             selectedInstanceKey={instanceId}
           />
